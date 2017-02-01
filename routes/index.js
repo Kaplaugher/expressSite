fa730a045a2792135f8d202338a44c73629e5b84
Kaplaugher <kaplaugher94@gmail.com>
2017-02-01 16:05:08 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +16,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/canvas', function(req, res, next){
   res.render('canvasGame', {});
-})
+});
+router.get('/weather', function(req, res, next){
+  var apikey = 'e312dbeb8840e51f92334498a261ca1d';
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&APPID="+apikey;
+
+  request.get(weatherUrl, (error, response, weatherData)=>{
+    console.log(weatherData);
+    weatherData = JSON.parse(weatherData);
+    res.render('weather', {weatherObject: weatherData})
+  });
+});
+
 
 module.exports = router;
